6fd9d6a848dd687c105e6d858d32966f67b04a18
Nadeem Choudhary <nadeem954039@gmail.com>
2024-12-28 07:49:09 +0000

    04_control_flow/one.js04_control_flow/switch.js04_control_flow/truthy.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"  //jo month no yaha dalege whai niche aayga but niche hone chiye ve for ex case 1,, 2, 3 eshe 
+
+switch (month) {
+    case "Jan":
+        console.log("January");        
+        break;
+    case "feb":
+        console.log("Feb");        
+        break;
+    case "march":
+        console.log("march");        
+        break;
+    case "april":
+        console.log("april");        
+        break;
+
+    default:
+        console.log("default case match");        
+        break;
+}
